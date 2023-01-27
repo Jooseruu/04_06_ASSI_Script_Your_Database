4d65f989efba4934be6c4189f264633d765509b5
recoby <arocarivera@cifpfbmoll.eu>
2023-01-27 19:41:07 +0100

    Creamos los ficheros de los scripts

---
+++ 1. Script crear tablas.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Extras (
+    extras_id INT PRIMARY KEY,
+    extras_name VARCHAR(255)
+);
+
+CREATE TABLE Categories (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255)
+);
+
+CREATE TABLE Bikes (
+    bike_id INT PRIMARY KEY,
+    category_id INT,
+    brand VARCHAR(30),
+    model VARCHAR(50),
+    developments VARCHAR(30),
+    price DECIMAL(5,2),
+    material VARCHAR(30),
+    frame_size INT,
+    groups TEXT,
+    avaibility BOOLEAN,
+    zip INT,
+    extras_id INT,
+    phone_number INT UNIQUE,
+    FOREIGN KEY (category_id) REFERENCES Categories,
+    FOREIGN KEY (extras_id) REFERENCES Extras
+);
+
+
+
